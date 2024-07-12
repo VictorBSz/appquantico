9f135faa9013e58cf9092485a4601f9b1437fcc0
Victor Souza <victor.souza0350@gmail.com>
2024-07-12 20:36:22 -0300

    First commit

---
+++ appquantico/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from "./components/Header/index"
+  
+function App() {
+  return (
+    <>
+      <div>
+      <Header />
+      <main>
+        {/* O conteúdo principal do seu aplicativo vai aqui */}
+      </main>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ appquantico/src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>QUANTIQ PROJETINHO</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/sobre">Sobre</a></li>
+          <li><a href="/contato">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
