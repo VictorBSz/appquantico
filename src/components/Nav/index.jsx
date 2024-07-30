dc5cfe17037f7cef6c95b33e7400ba9fbb08f65e
Victor Souza <victor.souza0350@gmail.com>
2024-07-29 22:23:58 -0300

    fix: Border-box padding fixed

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
   return (
     <header>
       <nav>
-        <ul className="horizontal-list">
+        <ul className="horizontal-nav">
           <h1><NavLink to="/">QUANTIQ PROJETINHO</NavLink></h1>
           <li><NavLink to="/products">Nossos Produtos</NavLink></li>
           <li><NavLink to="/">Monte seu PC</NavLink></li>
